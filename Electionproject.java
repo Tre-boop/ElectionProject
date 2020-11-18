2053268a5568481571f402d45cbe425cf5b19563
Tre-boop <trevaughn.clifton@gmail.com>
2020-11-18 17:15:01 -0500

    Add files via upload

---
+++ Electionproject.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Electionproject {
+    public static void main(String[] args){
+        int age = 0;
+        for (int i = 0; i < 100; i++){
+            age += 10;
+        }
+        System.out.println(age);
+    }
+}
